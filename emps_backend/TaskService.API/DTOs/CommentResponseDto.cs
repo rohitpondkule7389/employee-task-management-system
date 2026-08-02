dcaeaab3d5fc0256afa5f3ace5aaf7f5516b74f8
sarvesh-neekhra <neekhrasarvesh86@gmail.com@gmail.com>
2026-08-02 23:49:29 +0530

    Add Comment Dto and Update and Create

---
+++ emps_backend/TaskService.API/DTOs/CommentResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskService.API.DTOs
+{
+    public class CommentResponseDto
+    {
+        public int CommentId { get; set; }
+
+        public int TaskId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string CommentText { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
