0ae3bfc918e1e0fda73e08746a798f95b85f90c0
Rohit Pondkule <rohitpondkule01@gmail.com>
2026-08-02 13:10:28 +0530

    Add task and comment entity models

---
+++ emps_backend/TaskService.API/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskService.API.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string CommentText { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation Property
+        [ForeignKey(nameof(TaskId))]
+        public TaskItem Task { get; set; }
+    }
+}
