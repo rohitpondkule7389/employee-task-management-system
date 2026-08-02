80606d35fdcce2dff7ed47f3caf9da05b64cda61
sarvesh-neekhra <neekhrasarvesh86@gmail.com@gmail.com>
2026-08-02 23:47:12 +0530

    Implemented UserDto In Task Service

---
+++ emps_backend/TaskService.API/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskService.API.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+    }
+}
