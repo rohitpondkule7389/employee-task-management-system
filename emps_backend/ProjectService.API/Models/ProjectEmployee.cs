97856fc9d7f5f5f7773b52e182dfd50f25d2b13d
pawaryash150102-bit <pawaryash150102@gmail.com>
2026-08-02 22:57:23 +0530

    Project Service Model pages

---
+++ emps_backend/ProjectService.API/Models/ProjectEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectService.API.Models
+{
+    public class ProjectEmployee
+    {
+        [Key]
+        public int ProjectEmployeeId { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        public DateTime AssignedDate { get; set; } = DateTime.Now;
+
+        // Navigation Property
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+    }
+}
