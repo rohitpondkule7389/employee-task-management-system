29992ece0527adcf2ee976c496f105287f50b033
sarvesh-neekhra <neekhrasarvesh86@gmail.com@gmail.com>
2026-08-02 23:53:55 +0530

    Add task constants and API response helpers

---
+++ emps_backend/TaskService.API/Constants/TaskStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskService.API.Constants
+{
+    public static class TaskStatuses
+    {
+        public const string Pending = "Pending";
+
+        public const string InProgress = "In Progress";
+
+        public const string Completed = "Completed";
+    }
+}
