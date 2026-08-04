4c73c82110bb706d98e0b9aeb1a37d7d096a4990
Rohit Pondkule <rohitpondkule01@gmail.com>
2026-08-04 18:43:45 +0530

    Add authentication DTOs

---
+++ emps_backend/UserService.API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.API.DTOs
+{
+    public class RegisterDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
